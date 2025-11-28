89558b9eac09c07c6c268d3208b3fe5194d4e118
Brian Nguyen <bnguyen@cobank.com>
2025-11-28 14:51:28 -0700

    removing resources

--- main.tf
+++ main.tf
@@ -1,64 +1,64 @@
-## PUT TERRAFORM CLOUD BLOCK HERE!  ##
+# ## PUT TERRAFORM CLOUD BLOCK HERE!  ##
 
-terraform {
+# terraform {
   
-  cloud { 
+#   cloud { 
     
-    organization = "TerraformLearning1999" 
+#     organization = "TerraformLearning1999" 
 
-    workspaces { 
-      name = "tf-cloud-test" 
-    } 
-  } 
+#     workspaces { 
+#       name = "tf-cloud-test" 
+#     } 
+#   } 
   
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.50"
-    }
-  }
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.50"
+#     }
+#   }
 
-  required_version = ">= 1.4.0"
-}
+#   required_version = ">= 1.4.0"
+# }
 
-provider "aws" {
-  region = "us-east-2"
-}
+# provider "aws" {
+#   region = "us-east-2"
+# }
 
-# Data source to get the latest Ubuntu 22.04 LTS AMI for us-east-2
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical's AWS account ID
+# # Data source to get the latest Ubuntu 22.04 LTS AMI for us-east-2
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners      = ["099720109477"] # Canonical's AWS account ID
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
 
-# Data source to get the first available subnet in us-east-2
-data "aws_subnet_ids" "selected" {
-  vpc_id = data.aws_vpc.selected.id
-}
+# # Data source to get the first available subnet in us-east-2
+# data "aws_subnet_ids" "selected" {
+#   vpc_id = data.aws_vpc.selected.id
+# }
 
-# Data source to get the default VPC in us-east-2
-data "aws_vpc" "selected" {
-  default = true
-}
+# # Data source to get the default VPC in us-east-2
+# data "aws_vpc" "selected" {
+#   default = true
+# }
 
-resource "aws_instance" "tc_instance" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
-  subnet_id                   = tolist(data.aws_subnet_ids.selected.ids)[0]
-  associate_public_ip_address = true
+# resource "aws_instance" "tc_instance" {
+#   ami                         = data.aws_ami.ubuntu.id
+#   instance_type               = "t3.micro"
+#   subnet_id                   = tolist(data.aws_subnet_ids.selected.ids)[0]
+#   associate_public_ip_address = true
 
-  tags = {
-    Name = "TC-triggered-instance"
-  }
-}
+#   tags = {
+#     Name = "TC-triggered-instance"
+#   }
+# }
